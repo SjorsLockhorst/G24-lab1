0327f3247774a5c7106def56d91ceef1fe554518
nflechner <60731786+nflechner@users.noreply.github.com>
2022-09-13 17:45:28 +0200

    transforming the data into pd dataset.

--- extract.py
+++ extract.py
@@ -1,16 +1,34 @@
 """Module to extract and load data into a usuable format."""
 
+import pandas as pd
 
 def read_data():
     """Reads data from a path and returns the proper data structure."""
-    pass
+
+    f = open('data/dialog_acts.dat', 'r')
+    col1 = [] # creating a list for column 1 (dialog act labels)
+    col2 = [] # creating a list for column 2 (sentences)
+   
+    for i in f:
+        split_line = i.split(None, 1) #split each line into first word and rest of line. 
+        col1.append(split_line[0]) # append first word to list of dialog acts
+        col2.append(split_line[1].strip()) # append rest of lines to list of sentences, removing /n 
+
+    columns = {'col1': col1, 'col2': col2}
+    df = pd.DataFrame(data=columns) # passing the data into pandas dataframe
+    return df
 
 
 def split_data():
     """Splits data into train and test dataset."""
+
+    #either use numpy random or sklearn traintest split to split the data
+
     pass
 
 
 def create_dataset():
     """Creates dataset by reading and splitting dataset."""
     pass
+
+read_data()
--- test.py
+++
@@ -1,3 +0,0 @@
-# it's been a while, let's hope i can still do this
-
-print('hello')
