89ef7b3bdcc9d277083f86a7746794f7dd061fe0
Sjors Lockhorst <lockhorstsjors@gmail.com>
2022-09-20 21:07:51 +0200

    Added some more comments and features

--- states.py
+++ states.py
@@ -1,14 +1,19 @@
 import abc
+from typing import Optional
 
 from machine_learning import load_model
+from extract import create_restaurant_dataset
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
 
 log_reg = load_model("log_reg.pickle")
 
 
 class StateInterface(metaclass=abc.ABCMeta):
-    def __init__(self, number, next_states, end=False):
+    def __init__(self, number, end=False):
         self.number = number
-        self.next_states = next_states
         self.end = end
 
     @classmethod
@@ -24,51 +29,89 @@ class StateInterface(metaclass=abc.ABCMeta):
 
 
 class WelcomeState(StateInterface):
-    def __init__(self, number, next_states):
-        super().__init__(number, next_states)
+    def __init__(self, number):
+        super().__init__(number)
 
     def activate(self):
         print("Welcome user")
 
 
 class ThankYouState(StateInterface):
-    def __init__(self, number, next_states):
-        super().__init__(number, next_states)
+    def __init__(self, number):
+        super().__init__(number)
 
     def activate(self):
         print("Thanks!")
 
 
 class ByeState(StateInterface):
-    def __init__(self, number, next_states):
-        super().__init__(number, next_states, end=True)
+    def __init__(self, number):
+        super().__init__(number)
 
     def activate(self):
         print("Good bye")
 
 
+def query(data, expected):
+    col, value = expected
+    return data[data[col] == value]
+
+
+def query_information(data, information):
+    data = data.copy()
+    if information.pricerange:
+        data = query(data, ("pricerange", information.pricerange))
+    if information.area:
+        data = query(data, ("area", information.area))
+    if information.food:
+        data = query(data, ("food", information.food))
+    return data
+
+
+def entropy(arr):
+    p_values = arr.value_counts() / len(arr)
+    return -np.sum((np.log(p_values) / np.log(2)) * p_values)
+
+
+@dataclass
+class Information:
+    pricerange: Optional[str]
+    area: Optional[str]
+    food: Optional[str]
+
+
 def transition(
-    state: StateInterface, sentence: str, model=log_reg, verbose=False
-) -> StateInterface:
+    state: StateInterface,
+    sentence: str,
+    information: Information,
+    model=log_reg,
+    verbose=False,
+):
     dialog_act = model.predict([sentence.lower()])[0]
     if verbose:
         print(f"Dialog act: {dialog_act}")
         print(f"Previous state: {state}")
-        print(f"Next state: 'TODO: put next state'")
+        print("Next state: 'TODO: put next state'")
+    # Extract any information from user input
+    # Fill slots, pricerange, area, food
 
-    return state.next_states
+    return state, information
 
 
 if __name__ == "__main__":
     # Activate first state
-    # While state.end == False
-    bye = ByeState(1, None)
-    welcome = WelcomeState(1, bye)
+    data = create_restaurant_dataset()
+    print(query_information(data, Information("cheap", "west", None)))
+    # bye = ByeState(1)
+    # welcome = WelcomeState(1)
 
-    state = welcome
-    VERBOSE = True
-    while not state.end:
-        state.activate()
-        user_input = input()
-        state = transition(state, user_input, verbose=VERBOSE)
-    state.activate()
+    # current_state: StateInterface = welcome
+    # current_information = Information(None, None, None)
+    # VERBOSE = True
+    # while not current_state.end:
+    #     current_state.activate()
+    #     user_input = input()
+    #     current_state, current_information = transition(
+    #         current_state, user_input, current_information, verbose=VERBOSE
+    #     )
+    # current_state.activate()
