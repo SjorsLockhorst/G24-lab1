3efe6b0f75b8d67f831edf50d4e649d6d07c4a70
Sjors Lockhorst <lockhorstsjors@gmail.com>
2022-10-20 14:21:10 +0200

    Added function df_to_table directly instead of broken import

--- experiment.py
+++ experiment.py
@@ -7,8 +7,8 @@ import random
 import time
 
 from rich import print
+from rich.table import Table
 from rich.console import Console
-from rich_tools import df_to_table
 import pandas as pd
 
 from dialog_system import start
@@ -22,6 +22,36 @@ RESPID_PATH = os.path.join(RESULTS_DIR, "respondents.csv")
 
 respid_data = pd.read_csv(RESPID_PATH)
 queries_data = pd.read_csv(QUERY_PATH)
+
+
+def df_to_table(
+    pandas_dataframe: pd.DataFrame,
+    rich_table: Table = Table(),
+    show_index: bool = True,
+    index_name: Optional[str] = None,
+) -> Table:
+    """Convert a pandas.DataFrame obj into a rich.Table obj.
+    Args:
+        pandas_dataframe (DataFrame): A Pandas DataFrame to be converted to a rich Table.
+        rich_table (Table): A rich Table that should be populated by the DataFrame values.
+        show_index (bool): Add a column with a row count to the table. Defaults to True.
+        index_name (str, optional): The column name to give to the index column. Defaults to None, showing no value.
+    Returns:
+        Table: The rich Table instance passed, populated with the DataFrame values."""
+
+    if show_index:
+        index_name = str(index_name) if index_name else ""
+        rich_table.add_column(index_name)
+
+    for column in pandas_dataframe.columns:
+        rich_table.add_column(str(column))
+
+    for index, value_list in enumerate(pandas_dataframe.values.tolist()):
+        row = [str(index)] if show_index else []
+        row += [str(x) for x in value_list]
+        rich_table.add_row(*row)
+
+    return rich_table
 
 
 @dataclass
